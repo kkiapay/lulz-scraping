83259de88f11ac68a047bf977258dd33d734c524
ZEGUE Léonel <leonel.zegue@mehriso.com>
2019-05-07 20:54:08 +0100

    set host value to 0.0.0.0 dans add web process.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python run.py && APPLICATION_ENV="Production" gunicorn -k gevent run:app
+web: python run.py && APPLICATION_ENV="Production" gunicorn -k gevent run:app
--- run.py
+++ run.py
@@ -24,4 +24,4 @@ def hello_world():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
